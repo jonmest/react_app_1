d5296ce8aac3ef60b7cfc5670a17e5a57b241008
jonmest <jc222rz@student.lnu.se>
2019-12-10 22:37:06 +0100

    setting up foundation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Navbar from './components/layouts/Navbar';
+import './App.css';
+import Users from './components/users/Users';
 
-function App() {
-  return (
-    <div className="App">
-<h1>Hello from React</h1>
-    </div>
-  );
+class App extends Component{
+
+  render () {
+    return (
+      <div className="App">
+        <Navbar></Navbar>
+        <div className="container">
+        <Users></Users>
+
+        </div>
+      </div>
+    );
+  }
+
 }
 
 export default App;

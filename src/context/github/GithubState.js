6b84453c7e42ffe2a3b6c70b613bddeea0276aad
jonmest <jc222rz@student.lnu.se>
2019-12-13 22:49:21 +0100

    saving

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,25 @@
+import githubContext from './githubContext';
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import GithubContext from './githubContext';
+import GithubReducer from './githubReducer';
+import {
+    SEARCH_USERS,
+    SET_ALERT,
+    SET_LOADING,
+    CLEAR_USERS,
+    GET_REPOS,
+    GET_USER
+} from '../types'
+
+const GithubState = props => {
+    const initialState = {
+        users: [],
+        user: {},
+        repos: [],
+        loading: false
+    }
+
+    const [state, dispatch] = useReducer(GithubReducer, initialState);
+    
+}
